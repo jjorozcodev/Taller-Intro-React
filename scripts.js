adf9e5845ee5f3e07c4b706a7b270c1d87778c13
JJ <jj.orozcolopez.13@gmail.com>
2019-06-22 11:18:24 -0600

    Render Header with React
    Se sustituye todo el header por un elemento de react, aplicando buenas prácticas de ES6 en el documento de scripts para declarar los elementos de React.

--- scripts.js
+++ scripts.js
@@ -6,8 +6,14 @@
 // );
 
 // Usando JSX
-const elemento = <p className="may">
-                    ¡El inicio de toda <strong> una saga del infinito </strong> !
-                </p>
+const elemento = (
+    <header>
+        <h1>Universo Cinematográfico de Marvel</h1>
+        <p>El inicio de toda una <strong>saga del infinito</strong>.</p>
+        <p className="may">
+            ¡El inicio de toda <strong> una saga del infinito </strong> !
+        </p>
+    </header>
+);
 
-ReactDOM.render(elemento, document.getElementById('app'));
+ReactDOM.render(elemento, document.getElementById('encabezado'));
