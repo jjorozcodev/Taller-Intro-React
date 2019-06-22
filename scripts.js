4fc7558afa53576e96400827ceed93ab34790fd3
JJ <jj.orozcolopez.13@gmail.com>
2019-06-22 11:06:26 -0600

    Using babel and JSX
    Se agrega librería de babel y se modifica la importación del script como tipo babel.
    Además se cambia en el script la declaración del elemento párrafo y el anidado strong en él. Usando JSX.

--- scripts.js
+++ scripts.js
@@ -1,8 +1,13 @@
-const elemento = React.createElement(
-    'p',
-    {className: 'may', style: {backgroundColor: '#75010A', color: 'white'}},
-    '¡El inicio de toda una ',
-    React.createElement('strong', null, 'saga del infinito'), '!'
-);
+// const elemento = React.createElement(
+//     'p',
+//     {className: 'may', style: {backgroundColor: '#75010A', color: 'white'}},
+//     '¡El inicio de toda una ',
+//     React.createElement('strong', null, 'saga del infinito'), '!'
+// );
+
+// Usando JSX
+const elemento = <p className="may">
+                    ¡El inicio de toda <strong> una saga del infinito </strong> !
+                </p>
 
 ReactDOM.render(elemento, document.getElementById('app'));
