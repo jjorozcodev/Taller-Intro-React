6c0de6dcbbc2373de9cbcc45b491c8f21a51254c
JJ <jj.orozcolopez.13@gmail.com>
2019-06-22 11:40:16 -0600

    Render all the website
    Renderizando todo el contenido de la página para mostrarlo en un contenedor como un componente.
    Dentro del componente se llama al componente de encabezado y se corrigen palabras reservadas como "class" por "className".

--- scripts.js
+++ scripts.js
@@ -18,15 +18,48 @@
 
 // Usando JSX y funciones para crear componentes
 function Header() {
-    return (
-        <header>
-            <h1>Universo Cinematográfico de Marvel</h1>
-            <p>El inicio de toda una <strong>saga del infinito</strong>.</p>
-            <p className="may">
-                ¡El inicio de toda <strong> una saga del infinito </strong> !
-            </p>
-        </header>
-    )
-};
+     return (
+         <header>
+             <h1>Universo Cinematográfico de Marvel</h1>
+             <p>El inicio de toda una <strong>saga del infinito</strong>.</p>
+             <p className="may">
+                 ¡El inicio de toda <strong> una saga del infinito </strong> !
+             </p>
+         </header>
+     )
+ };
 
-ReactDOM.render(<Header />, document.getElementById('encabezado'));
+// RENDERIZAR TODA LA PÁGINA CON REACT
+function ContenedorApp() {
+    return (
+        <React.Fragment>
+            <Header />
+            <h2>FASE ONE</h2>
+            <section>
+                <article>
+                    <h3>Ironman</h3>
+                    <div className="year">Publicada en 2008</div>
+                    <img alt="Portada de Ironman" src="https://cl.buscafs.com/www.tomatazos.com/public/uploads/images/170592.jpg" />
+                </article>
+                <article>
+                    <h3>Thor</h3>
+                    <div className="year">Publicada en 2011</div>
+                    <img alt="Portada de Thor" src="https://cl.buscafs.com/www.tomatazos.com/public/uploads/images/170606.jpg" />
+                </article>
+                <article>
+                    <h3>Capitán América: El primer vengador</h3>
+                    <div className="year">Publicada en 2011</div>
+                    <img alt="Portada de Capitán América: El primer vengador" src="https://cl.buscafs.com/www.tomatazos.com/public/uploads/images/170605.jpeg" />
+                </article>
+                <article>
+                    <h3>The Avengers</h3>
+                    <div className="year">Publicada en 2012</div>
+                    <img alt="Portada de The Avengers" src="https://cl.buscafs.com/www.tomatazos.com/public/uploads/images/170610.jpg" />
+                </article>
+            </section>
+        </React.Fragment>
+
+    );
+}
+
+ReactDOM.render(<ContenedorApp />, document.getElementById('app'));
