2adcbb23af7fb43b8afa413227b608a85929736d
JJ <jj.orozcolopez.13@gmail.com>
2019-06-22 10:57:34 -0600

    Setting Styles with React
    Una manera de aplicar estilos en línea a un elemento con React

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 const elemento = React.createElement(
     'p',
-    {className: 'may'},
+    {className: 'may', style: {backgroundColor: '#75010A', color: 'white'}},
     '¡El inicio de toda una ',
     React.createElement('strong', null, 'saga del infinito'), '!'
 );
