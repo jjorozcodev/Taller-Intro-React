2100c473368b1f8bb220591ff55a0506de8cdaf8
JJ <jj.orozcolopez.13@gmail.com>
2019-06-22 15:11:01 -0600

    Adding array to state dinamically
    Se agrega con el botón un arreglo de nuevas películas y se oculta el botón.
    Se agrega la funcionalidad de votos y ordenamiento del arreglo para pasarlo al state y actualizar el renderizado con React.

--- scripts.js
+++ scripts.js
@@ -1,21 +1,3 @@
-// const elemento = React.createElement(
-//     'p',
-//     {className: 'may', style: {backgroundColor: '#75010A', color: 'white'}},
-//     '¡El inicio de toda una ',
-//     React.createElement('strong', null, 'saga del infinito'), '!'
-// );
-
-// Usando JSX
-// const elemento = (
-//     <header>
-//         <h1>Universo Cinematográfico de Marvel</h1>
-//         <p>El inicio de toda una <strong>saga del infinito</strong>.</p>
-//         <p className="may">
-//             ¡El inicio de toda <strong> una saga del infinito </strong> !
-//         </p>
-//     </header>
-// );
-
 // Usando JSX y funciones para crear componentes
 function Header() {
     return (
@@ -28,24 +10,12 @@ function Header() {
     )
 };
 
-// Componente Película
-class Movie extends React.Component {
-    render(){
-        return (
-            <article>
-                <h3>{this.props.nombre}</h3>
-                <div className="year">Publicada en {this.props.year}</div>
-                <img alt="Portada de Ironman" src={this.props.foto} />
-            </article>
-        )
-    }
-};
-
 const seed = [
     {
         id: "tt0117705",
         name: "Ironman",
         year: "2008",
+        votes: 5,
         cover:
             "https://cl.buscafs.com/www.tomatazos.com/public/uploads/images/170592.jpg"
     },
@@ -53,6 +23,7 @@ const seed = [
         id: "tt0158811",
         name: "Thor",
         year: "2011",
+        votes: 4,
         cover:
             "https://cl.buscafs.com/www.tomatazos.com/public/uploads/images/170606.jpg"
     },
@@ -60,6 +31,7 @@ const seed = [
         id: "tt0062622",
         name: "Capitán América: El primer vengador",
         year: "2011",
+        votes: 3,
         cover:
             "https://cl.buscafs.com/www.tomatazos.com/public/uploads/images/170605.jpeg"
     },
@@ -67,10 +39,52 @@ const seed = [
         id: "tt0062633",
         name: "The Avengers",
         year: "2012",
+        votes: 2,
         cover:
             "https://cl.buscafs.com/www.tomatazos.com/public/uploads/images/170610.jpg"
     }
 ];
+
+const extraFilms = [
+    {
+        id: "tt12345",
+        name: "Hulk",
+        year: 2008,
+        votes: 0,
+        cover: "https://cl.buscafs.com/www.tomatazos.com/public/uploads/images/170597.jpg"
+    },
+    {
+        id: "ir212345",
+        name: "Ironman 2",
+        year: 2010,
+        votes: 1,
+        cover: "https://cl.buscafs.com/www.tomatazos.com/public/uploads/images/170599.jpg"
+    },
+    {
+        id: "ir312345",
+        name: "Ironman 3",
+        year: 2013,
+        votes: 1,
+        cover: "https://cl.buscafs.com/www.tomatazos.com/public/uploads/images/170611.jpg"
+    }
+  ];
+  
+// Componente Película
+class Movie extends React.Component {
+    render(){
+        return (
+            <article>
+                <h3>{this.props.nombre}</h3>
+                <div className="year">Publicada en {this.props.year}</div>
+                <button id={this.props.id} onClick={this.props.handleClick}>
+                        +1 voto
+                </button>
+                <div className="votes">{this.props.votes} votos</div>
+                <img alt={"Portada de " + this.props.nombre} src={this.props.foto} />
+            </article>
+        )
+    }
+};
 
 // RENDERIZAR TODA LA PÁGINA CON REACT
 class ContenedorApp extends React.Component {
@@ -79,47 +93,75 @@ class ContenedorApp extends React.Component {
         super(props);
 
         this.state = {
-            peliculas: [].concat(seed)
+            peliculas: [].concat(seed),
+            showAddMoviesButton: true
         }
 
         this.handleClick = this.handleClick.bind(this);
+        this.handleAddMoviesClick = this.handleAddMoviesClick.bind(this);
     }
 
     handleClick(event) {
         event.preventDefault();
+    
+        const clickedFilmId = event.target.id;
+        console.log(event.target.id);
+    
+        this.setState(function(prevState) {
+          const newFilms = prevState.peliculas.map(function(film) {
+            if (film.id === clickedFilmId) {
+              return Object.assign({}, film, { votes: film.votes + 1 });
+            } else {
+              return film;
+            }
+          });
+    
+          return { peliculas: newFilms };
+        });
+    }
+
+    handleAddMoviesClick(event) {
+        event.preventDefault();
+
+        const clickedFilmId = event.target.id;
+
         console.log(this.state.peliculas);
 
-        const nuevaPeli = {
-            id: "tt12345",
-            name: "Hulk",
-            year: 2008,
-            cover: "https://cl.buscafs.com/www.tomatazos.com/public/uploads/images/170597.jpg"
-        }
-
-        // Funciona de ésta manera, sin embargo no garantiza que en un bombardeo de clicks se obtenga el último valor de state.
-        //const lista = this.state.peliculas.concat(nuevaPeli);
-
-        this.setState(function(prevState)
-        {
-            
-            return {peliculas: prevState.peliculas.concat(nuevaPeli)};
-        })
-      }
+        this.setState({
+            showAddMoviesButton:false
+        });
+        this.setState(
+            function(prevState){
+                return { peliculas: prevState.peliculas.concat(extraFilms)}
+            }
+        );        
+    }
 
     render() {
+        const sortedFilms = this.state.peliculas.sort((a, b) => b.votes - a.votes);
+
         return (
             <React.Fragment>
                 <Header />
-                 <button type="button" onClick={this.handleClick}>Agregar Película</button>
+                {this.state.showAddMoviesButton && (
+                    <button onClick={this.handleAddMoviesClick}>
+                    Agregar películas
+                    </button>
+                )}
+                 {/* <button type="button" onClick={this.handleClick}>Agregar Película</button> */}
                 <h2>FASE ONE</h2>
+
                 <section>
-                    {this.state.peliculas.map(function (film) {
+                    {sortedFilms.map(film => {
                         return (
                             <Movie
                                 key={film.id}
+                                id={film.id}
                                 nombre={film.name}
                                 year={film.year}
                                 foto={film.cover}
+                                votes={film.votes}
+                                handleClick={this.handleClick}
                             />
                         );
                     })}
